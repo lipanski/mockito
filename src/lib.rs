2fc01b1f7c08e168136d6eb6e039dde64a6989b2
Florin Lipan <florinlipan@gmail.com>
2018-09-13 16:32:47 +0200

    Document Matcher::Or inside lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -328,6 +328,27 @@
 //!     .create();
 //! ```
 //!
+//! # The `Or` matcher
+//!
+//! The `Matcher::Or` construct takes two ordinary matchers as arguments and will be enabled
+//! if at least one of the provided arguments match the request.
+//!
+//! ## Example
+//!
+//! ```
+//! use mockito::mock;
+//!
+//! // Will match requests to POST / whenever the request body is either `hello=world` or `{"hello":"world"}`
+//! let _m = mock("POST", "/")
+//!     .match_body(
+//!         Matcher::Or(
+//!             Matcher::Exact("hello=world"),
+//!             Matcher::JsonString("{\"hello\":\"world\"}".to_string()
+//!         )
+//!      )
+//!     .create();
+//!```
+//!
 //! # Non-matching calls
 //!
 //! Any calls to the Mockito server that are not matched will return *501 Not Implemented*.
