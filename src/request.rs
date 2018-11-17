1c784b377c5891f00d7f20f02606737f75bc5fa4
sjj <5555776+sterlingjensen@users.noreply.github.com>
2018-11-17 04:30:13 -0600

    Fix Windows build and doctest failures.
    Fixes #41 and #36
    
    Substitute [http_muncher] with [httparse]

--- src/request.rs
+++ src/request.rs
@@ -5,11 +5,10 @@ use std::convert::From;
 use std::default::Default;
 use std::net::TcpStream;
 use std::fmt;
-use http_muncher::{Parser, ParserHandler};
 
 #[derive(Debug)]
 pub struct Request {
-    version: (u16, u16),
+    version: (u8, u8),
     pub method: String,
     pub path: String,
     pub headers: Vec<(String, String)>,
@@ -37,10 +36,6 @@ impl Request {
         self.headers.iter().filter(|&&(ref field, _)| field == searched_field)
             .map(|&(_, ref value)| value.as_str())
             .collect()
-    }
-
-    fn is_parsed(&self) -> bool {
-        self.is_parsed
     }
 
     fn record_last_header(&mut self) {
@@ -71,111 +66,52 @@ impl Default for Request {
 impl<'a> From<&'a mut TcpStream> for Request {
     fn from(stream: &mut TcpStream) -> Self {
         let mut request = Request::default();
-        let mut parser = Parser::request();
-        let mut first_read = true;
+        let mut buf = [0; 1024];
 
-        loop {
-            if request.is_parsed() { break; }
+        let rlen = match stream.read(&mut buf) {
+            Err(e) => Err(e.to_string()),
+            Ok(0)  => Err("Nothing to read.".into()),
+            Ok(i)  => Ok(i)
+        }.map_err(|e| request.error = Some(e)).unwrap_or(0);
+        if rlen == 0 {
+            return request;
+        }
 
-            let mut buffer = [0; 1024];
-            let read_length = stream.read(&mut buffer).unwrap_or(0);
-
-            if read_length == 0 {
-                if first_read { request.error = Some("Nothing to read.".to_string()); }
-                break;
+        let mut headers = [httparse::EMPTY_HEADER; 16];
+        let mut req = httparse::Request::new(&mut headers);
+        let _ = req.parse(&buf).map_err(|e|{
+            request.error = Some(e.to_string());
+        }).and_then(|p| {
+            if let Some(a @ 0 ... 1) = req.version { request.version = (1,a) }
+            if let Some(a)           = req.method  { request.method += a }
+            if let Some(a)           = req.path    { request.path += a }
+            for h in req.headers {
+                request.last_header_field = Some(h.name.to_lowercase());
+                request.last_header_value = Some(String::from_utf8_lossy(h.value).into());
+                request.record_last_header();
             }
-
-            first_read = false;
-
-            let parse_length = parser.parse(&mut request, (&buffer).chunks(read_length).nth(0).unwrap());
-            if parse_length == 0 || parser.has_error() { break; }
-        }
-
-        if parser.has_error() {
-            request.error = Some(parser.error().to_string());
-        } else {
-            request.version = parser.http_version();
-            request.method = parser.http_method().to_string().to_uppercase();
-        }
-
+            if let httparse::Status::Complete(plen) = p {
+                request.is_parsed = true;
+                request.body.extend_from_slice(&buf[plen..rlen]);
+            }
+            Ok(())
+        });
         request
-    }
-}
-
-impl ParserHandler for Request {
-    fn on_message_begin(&mut self, parser: &mut Parser) -> bool {
-        !parser.has_error()
-    }
-
-    fn on_url(&mut self, parser: &mut Parser, value: &[u8]) -> bool {
-        self.path.push_str(str::from_utf8(value).unwrap());
-
-        !parser.has_error()
-    }
-
-    fn on_header_field(&mut self, parser: &mut Parser, value: &[u8]) -> bool {
-        self.record_last_header();
-
-        if self.last_header_field.is_none() {
-            self.last_header_field = Some(String::new());
-        }
-
-        (*self.last_header_field.as_mut().unwrap()).push_str(str::from_utf8(value).unwrap());
-
-        !parser.has_error()
-    }
-
-    fn on_header_value(&mut self, parser: &mut Parser, value: &[u8]) -> bool {
-        if self.last_header_value.is_none() {
-            self.last_header_value = Some(String::new());
-        }
-
-        (*self.last_header_value.as_mut().unwrap()).push_str(str::from_utf8(value).unwrap());
-
-        !parser.has_error()
-    }
-
-    fn on_headers_complete(&mut self, parser: &mut Parser) -> bool {
-        self.record_last_header();
-
-        !parser.has_error()
-    }
-
-    fn on_body(&mut self, parser: &mut Parser, value: &[u8]) -> bool {
-        self.body.extend(value);
-
-        !parser.has_error()
-    }
-
-    fn on_message_complete(&mut self, parser: &mut Parser) -> bool {
-        self.is_parsed = true;
-
-        !parser.has_error()
     }
 }
 
 impl fmt::Display for Request {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut formatted = String::new();
-
-        formatted.push_str("\r\n");
-        formatted.push_str(&self.method);
-        formatted.push_str(" ");
-        formatted.push_str(&self.path);
-        formatted.push_str("\r\n");
+        write!(f, "\r\n{} {}\r\n", &self.method, &self.path)?;
 
         for &(ref key, ref value) in &self.headers {
-            formatted.push_str(key);
-            formatted.push_str(": ");
-            formatted.push_str(value);
-            formatted.push_str("\r\n");
+            write!(f, "{}: {}\r\n", key, value)?;
         }
 
-        if self.body.len() > 0 {
-            formatted.push_str(&String::from_utf8_lossy(&self.body));
-            formatted.push_str("\r\n");
+        if !self.body.is_empty() {
+            write!(f, "{}\r\n", &String::from_utf8_lossy(&self.body))
+        } else {
+            write!(f, "")
         }
-
-        write!(f, "{}", formatted)
     }
 }
